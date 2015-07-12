43cccb2c2163ed681dc22ccf042f75b354955ed8
LauraKirby <laurakirby26@gmail.com>
2015-07-11 21:46:18 -0700

    all routes written in routes.rb, rake routes worked

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get '/pins', to: 'pins#index'
   get '/pins/new', to: 'pins#new'
   post '/pins', to: 'pins#create'
-  # get '/pins/:id', to: 'pins#show', as: 'pin'
-  # put '/pins/:id', to: 'pins#update'
-  # patch '/pins/:id', to: 'pins#update'
-  # get '/pins/:id/edit', to: 'pins#edit'
+  get '/pins/:id', to: 'pins#show', as: 'pin'
+  put '/pins/:id', to: 'pins#update'
+  patch '/pins/:id', to: 'pins#update'
+  get '/pins/:id/edit', to: 'pins#edit'
   delete '/pins/:id', to: 'pins#destory'
   
 end
